3026c3db29c1441745e043edf1e544571c82bdc3
Ankit Kanyal <35369589+ANKIT9761@users.noreply.github.com>
2023-02-17 22:46:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 import "./App.css";
 
-import { CarouselSlider } from "./components/Carousel/CarouselSlider";
 import { Navbar } from "./components/Navbar/Navbar";
 
 // import MailForm from "./components/MailForm";
@@ -15,9 +14,6 @@ function App() {
     <>
       {/* <Home /> */}
       {/* <About/> */}
-
-      {/* <Navbar />
-      <CarouselSlider /> */}
       {/* <MailForm /> */}
       <Form />
     </>
